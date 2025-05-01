d99b0e5206013fac3e0aaa0134d66ab7a9fc0335
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2025-05-01 20:38:12 -0300

    Refactor: removed ros dependency

--- ros2_ws/src/motor_controller/motor_controller_node.py
+++ ros2_ws/src/motor_controller/motor_controller_node.py
@@ -1,74 +1,38 @@
-import rclpy
-from rclpy.node import Node
-from geometry_msgs.msg import Twist
-import pigpio
+from gpio.gpio_controller import GPIOController
 
-class MotorController(Node):
-    def __init__(self):
-        super().__init__('motor_controller')
+class MotorController:
+    def __init__(self, pwm_left, in1, in2, pwm_right, in3, in4):
+        self.gpio = GPIOController()
+        self.pins = {
+            "pwm_left": pwm_left,
+            "in1": in1,
+            "in2": in2,
+            "pwm_right": pwm_right,
+            "in3": in3,
+            "in4": in4,
+        }
+        self.gpio.setup_output(self.pins.values())
 
-        self.pi = pigpio.pi()
-        self.pwm_left = 18
-        self.in1_left = 23
-        self.in2_left = 24
-
-        self.pwm_right = 17
-        self.in1_right = 22
-        self.in2_right = 27
-
-        for pin in [self.pwm_left, self.in1_left, self.in2_left,
-                     self.pwm_right, self.in1_right, self.in2_right]:
-            self.pi.set_mode(pin, pigpio.OUTPUT)
-
-        self.subcription = self.create_subscription(
-            Twist,
-            '/cmd_vel',
-            self.vel_callback,
-            10
-        )
-
-        self.get_logger().info("Motor Controller Node has been started.")
-
-    def vel_callback(self, msg):
-        v = msg.linear.x
-        w = msg.angular.z
-
-        left_speed = v - w
-        right_speed = v + w
-
-        self.execute_commando_motor(left_speed, self.in1_left, self.in2_left, self.pwm_left)
-        self.execute_commando_motor(right_speed, self.in1_right, self.in2_right, self.pwm_right)
-
-    def execute_commando_motor(self, speed, in1, in2, pwm):
+    def _control_motor(self, speed, in_a, in_b, pwm_pin):
         if speed > 0:
-            self.pi.write(in1, 1)
-            self.pi.write(in2, 0)
+            self.gpio.write(in_a, True)
+            self.gpio.write(in_b, False)
         elif speed < 0:
-            self.pi.write(in1, 0)
-            self.pi.write(in2, 1)
+            self.gpio.write(in_a, False)
+            self.gpio.write(in_b, True)
         else:
-            self.pi.write(in1, 0)
-            self.pi.write(in2, 0)
-        
+            self.gpio.write(in_a, False)
+            self.gpio.write(in_b, False)
+
         duty = min(abs(speed), 1.0) * 255
-        self.pi.set_PWM_dutycycle(pwm, duty)
+        self.gpio.set_pwm(pwm_pin, duty)
 
-    def destroy_node(self):
-        self.get_logger().info("Stopping motors...")
-        self.pi.set_PWM_dutycycle(self.pwm_left, 0)
-        self.pi.set_PWM_dutycycle(self.pwm_right, 0)
-        super().destroy_node()
+    def control_left_side(self, speed):
+        self._control_motor(speed, self.pins["in1"], self.pins["in2"], self.pins["pwm_left"])
 
-def main(args=None):
-    rclpy.init(args=args)
-    motor_controller = MotorController()
-    try:
-        rclpy.spin(motor_controller)
-    except KeyboardInterrupt:
-        pass
-    finally:
-        motor_controller.destroy_node()
-        rclpy.shutdown()
+    def control_right_side(self, speed):
+        self._control_motor(speed, self.pins["in3"], self.pins["in4"], self.pins["pwm_right"])
 
-if __name__ == '__main__':
-    main()
+    def stop(self):
+        self.gpio.stop_pwm(self.pins["pwm_left"])
+        self.gpio.stop_pwm(self.pins["pwm_right"])
