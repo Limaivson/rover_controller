2621da3b402a71de518ff04a90d4f8c76371880d
Ivson De Lima Santos <124381236+Limaivson@users.noreply.github.com>
2025-05-04 17:15:48 -0300

    Refactor: control motors

--- rover_control/rover_controller.py
+++ rover_control/rover_controller.py
@@ -17,7 +17,37 @@ class RoverController(Node):
     def cmd_vel_callback(self, msg):
         linear = msg.linear.x
         angular = msg.angular.z
-        speed_left = linear - angular
-        speed_right = linear + angular
+
+        # Movimento reto
+        if linear != 0.0 and angular == 0.0:
+            speed_left = linear
+            speed_right = linear
+
+        # Giro no próprio eixo
+        elif linear == 0.0 and angular != 0.0:
+            speed_left = -angular
+            speed_right = angular
+
+        # Curvas (um motor para, outro anda)
+        elif linear != 0.0 and angular != 0.0:
+            speed_left = linear - angular
+            speed_right = linear + angular
+
+        # Parado
+        else:
+            speed_left = 0.0
+            speed_right = 0.0
+
         self.motor_driver.control_left_side(speed_left)
         self.motor_driver.control_right_side(speed_right)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = RoverController()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
